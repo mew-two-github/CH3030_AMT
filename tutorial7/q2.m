89c8a0d4258359aa0b4723baf86532c594132fe8
vishal <subhavishal@yahoo.co.in>
2020-10-09 08:07:31 +0530

    tut7 code complete

--- tutorial7/q2.m
+++ tutorial7/q2.m
@@ -1,17 +1,22 @@
 clear; close all;
-kl = 2.25*10^-3;
-wmin = (3.5-0.1)/(50*0.1^(0.32));
-w = 2*wmin;
+kl = 2.25*10^-5;%m/s
+L = 10;%Litres
+wmin = (3.5-0.1)/(50*0.1^(0.32))*L;%g
+w = 2*wmin;%g
 ci = 3.5;%mg/L
+K = 50;%q should be in mg/g, c in mg/L
+a = 5;%m^2/kg
 time = integral(@dtbydc,3.5,0.1);
+t_inhours = time/3600;
 function val = dtbydc(c)
     kl = 2.25*10^-5;%m/s
-    wmin = (3.5-0.1)/(50*0.1^(0.32));%g
+    L = 10;%Litres
+    wmin = (3.5-0.1)/(50*0.1^(0.32))*L;%g
     w = 2*wmin;%g
     ci = 3.5;%mg/L
     K = 50;%q should be in mg/g, c in mg/L
     a = 5;%m^2/kg
-    L = 10;%Litres
     %units of val 
-    val = -1./(kl*a.*(c-((ci-c)/(w*K*L)).^(1/0.32)));
+    %c is the conc in solution, second term is the ideal c in soln
+    val = -1./(kl*(a*w/L).*(c-((ci-c)*L/(w*K)).^(1/0.32))); 
 end
--- tutorial7/q3.m
+++ tutorial7/q3.m
@@ -2,6 +2,7 @@ clear; close all;
 %% Given data
 u_superficial = 0.29;
 t = [9.5,19,21,25.7,34.3,39,42,46.7,51.4,56.2,64.7,68.6,72.4,77.1,84.8,97.1,104.7,108.6];
+te = 108.6;
 c = [0,0.018,0.037,0.083,0.287,0.435,0.491,0.62,0.713,0.768,0.852,0.935,0.952,0.963,0.97,0.987,0.991,1];
 L = 0.2;
 ci = 0.11;
@@ -15,7 +16,7 @@ ts = 230;
 ar_diff = zeros(n*10-1,1);
 l = length(time);
 for i = 2:l-1
-    ar_diff(i) = trapz(time(1:i),ppval(pp,time(1:i)))+trapz(time(i:l),ppval(pp,time(i:l)))-(108.6-time(i));
+    ar_diff(i) = trapz(time(1:l),ppval(pp,time(1:l)))-(108.6-time(i));
     if min > abs(ar_diff(i))
         min = abs(ar_diff(i));
         ts = time(i);
@@ -23,13 +24,14 @@ for i = 2:l-1
 end
 us = L/ts*60; %m/h
 LUB = L*(1-t_break/ts);
-MTZ = 2*LUB;%Since curve is symmetric
+MTZ = (te-t_break)/te*L;%Since curve is NOT symmetric
 %% Part b
-V = 8000/60/60;%m^3/s
+V = 3000/60/60;%m^3/s
 D = sqrt(4*V/(u_superficial*pi));
 t_cycle = 8;
+bed_density = 700;
 L_used = 8*us;
-L_scaleup = LUB*2 + L_used; 
+L_scaleup = MTZ + L_used; 
 sol_adsorbed = V*ci*t_cycle*3600;
-avg_loading = sol_adsorbed/L_scaleup;
-max_loading = sol_adsorbed/L_used;
+avg_loading = sol_adsorbed/(L_scaleup*bed_density*pi*D^2/4);
+max_loading = sol_adsorbed/(L_used*bed_density*pi*D^2/4);
